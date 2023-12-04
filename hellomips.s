0ad62ef03c93da4b45e5608ee71ce755ff7d4de6
Le'ale Addison <110490155+lealeaddison@users.noreply.github.com>
2023-12-03 22:02:14 -0500

    entering prompts for users

--- hellomips.s
+++ hellomips.s
@@ -1,6 +1,8 @@
     .data
-
+promptL:    .asciiz "Enter the value of L greater than 20: "
+promptM:    .asciiz "Enter the value of M greater than 20: "
+promptN:    .asciiz "Enter the value of N greater than 20: "
+error:  .asciiz "This is an illegal number!\n"
     .text
 
 main:
-    
