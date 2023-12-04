f7ac4f7b98fccd2cdf3a2bd226eca224fec9b3e3
Le'ale Addison <110490155+lealeaddison@users.noreply.github.com>
2023-12-03 22:13:51 -0500

    creating gcd_loop and if $t1 is 0 exit loop

--- hellomips.s
+++ hellomips.s
@@ -8,4 +8,11 @@ error:  .asciiz "This is an illegal number!\n"
 gcd:
     move $t0, $a0 # copy first num to $t0
     move $t1, $a1 # copy second num to $t1
+
+gcd_loop:
+    beq $t1, $zero, gcd_done
+    rem $t2, $t0, $t1
+    move $t0, $t1 # $t0 = $t1
+    move $t1, $t2 # $t1 = $t2
+    j gcd_loop
 main:
