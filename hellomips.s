c30d72ebf8d6ca878fe54b985d91840264057b0b
Le'ale Addison <110490155+lealeaddison@users.noreply.github.com>
2023-12-03 22:07:28 -0500

    copy first and second nums to $t0 and $t1

--- hellomips.s
+++ hellomips.s
@@ -3,6 +3,9 @@ promptL:    .asciiz "Enter the value of L greater than 20: "
 promptM:    .asciiz "Enter the value of M greater than 20: "
 promptN:    .asciiz "Enter the value of N greater than 20: "
 error:  .asciiz "This is an illegal number!\n"
-    .text
 
+    .text
+gcd:
+    move $t0, $a0 # copy first num to $t0
+    move $t1, $a1 # copy second num to $t1
 main:
